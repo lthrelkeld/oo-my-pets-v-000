d9eaf74d046f0817bb87484a7e3c2f2b074939b2
Elizabeth Karst <lkarst@outlook.com>
2018-08-25 20:58:07 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,7 @@
 class Owner
-  # code goes here
+  #knows about all pets
+  #can buy a pet
+  #can set the name of a pet (which the pet cant change)
+  #can change a pets mood through walking, feeding, or playing with it
+  #can sell all of its pets
 end
