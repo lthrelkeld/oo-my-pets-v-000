eca1d03eb22b431fc88a4c1e2086677c929e5ecb
Elizabeth Karst <lkarst@outlook.com>
2018-08-25 21:01:57 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,9 @@
 class Owner
+
+  @@owners = []
+
   #knows about all pets
-  @@pets = []
-
   #can buy a pet
-
   #can set the name of a pet (which the pet cant change)
   #can change a pets mood through walking, feeding, or playing with it
   #can sell all of its pets
